a0a5cc66c285d4e8f3c863ba6226017daf4e1f43
Carlos Herrera <carloshv93@gmail.com>
2016-07-01 16:20:36 -0600

    Hola Mundo-Ciclo For

---
+++ Vector-For/Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo{
+	//Tipo[] nombreVariable
+	//[0][1][2][3]...[n]
+	public static void main(String[] args){
+		for(int i=0;i<args.length;i++){			
+			System.out.println("Hola "+args[i].toString());	
+		}	
+	}
+}
