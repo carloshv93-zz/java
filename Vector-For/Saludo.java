12c4899a8bd7b5e21030b92a79e13253c9f31829
Carlos Herrera <carloshv93@gmail.com>
2016-07-01 16:56:51 -0600

    Ciclo For-Vectores-args

--- Vector-For/Saludo.java
+++ Vector-For/Saludo.java
@@ -1,9 +1,13 @@
 public class Saludo{
-	//Tipo[] nombreVariable
-	//[0][1][2][3]...[n]
+	//Tipo[] nombre;
+	//[0][1][2]...[n]
 	public static void main(String[] args){
-		for(int i=0;i<args.length;i++){			
-			System.out.println("Hola "+args[i].toString());	
-		}	
+			for(int i=0;i<args.length;i++){
+				System.out.println("Hola " + args[i].toString());
+			}
 	}
 }
+// i= 0
+// i= 1
+// i= 2
+// i= 3
